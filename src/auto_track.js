a6fb6e0fe8890b0b6d77dc1c87b946f38361f9c5
Petar Shomov <petar@activitystream.com>
2015-08-13 16:25:29 +0000

    detecting opened sections on startup

--- src/auto_track.js
+++ src/auto_track.js
@@ -5,7 +5,7 @@ module.exports = {
 		var locationHashChanged = function (oldHash, newHash) {
 			asa('sectionentered', newHash.substr(1));
 		}
-		var storedHash = window.location.hash;
+		var storedHash = '';
 		window.setInterval(function () {
 			if (window.location.hash != storedHash) {
 				var newHash = window.location.hash;
