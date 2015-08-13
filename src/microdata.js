f4a09aabf9d0741d0e6b4ac86fe20ef5610a3035
Petar Shomov <petar@activitystream.com>
2015-08-13 12:26:58 +0000

    adding itemref parsing too, seems to cover the microdata for our first customer

--- src/microdata.js
+++ src/microdata.js
@@ -1,30 +1,44 @@
 var debug = require('./debug');
 
+var collectReferencedProperties = function (element, item) {
+	var refString = element.attr('itemref');
+	if (typeof refString !== 'undefined') {
+		var refs = refString.split(' ');
+		for (var i = 0; i < refs.length; i++) {
+			var ref = refs[i];
+			collectProperties($('#' + ref), item);
+		}
+	}
+};
+
 var collectComplexProperty = function (element) {
 	var item = {
 		"type": element.attr("itemtype"),
 		"properties": {}
 	};
+	collectReferencedProperties(element, item)
 	collectProperties(element, item);
 	return item;
-}
+};
+
 var collectSimpleProperty = function (el) {
 	var tag = el.prop('tagName');
-	switch(tag){
-		case 'TIME' : return el.attr('datetime');
-		case 'A' : 
-		case 'LINK' : 
+	switch (tag) {
+		case 'TIME': return el.attr('datetime');
+		case 'A':
+		case 'LINK':
 			return el.attr('href');
 		default:
 			return el.prop("content") || el.text() || el.attr("src");
 	}
-	
-}
+
+};
+
 var collectProperties = function (el, item) {
 	el.children().each(function (_, c) {
 		var child = $(c);
 		var prop = child.attr('itemprop');
-		if (prop) {
+		if (typeof prop === 'string') {
 			if (typeof child.attr('itemscope') !== 'undefined') {
 				item.properties[prop] = collectComplexProperty(child);
 			} else {
@@ -37,6 +51,7 @@ var collectProperties = function (el, item) {
 		}
 	});
 };
+
 var findTopLevelItems = function (el) {
 	var items = [];
 
@@ -48,12 +63,7 @@ var findTopLevelItems = function (el) {
 			if (typeof prop !== 'undefined') {
 				return;
 			} else {
-				var topLevelItem = {
-					"type": el.attr("itemtype"),
-					"properties": {}
-				};
-				collectProperties(el, topLevelItem);
-				items.push(topLevelItem);
+				items.push(collectComplexProperty(el));
 			}
 		} else {
 			el.children().each(function (_, c) {
