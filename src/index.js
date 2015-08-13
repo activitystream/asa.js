14c814fc8c6c8c152cb72907885bc7245c02bf55
Petar Shomov <petar@activitystream.com>
2015-08-13 15:29:34 +0000

    adding safety to avoid interfering with the customer experience

--- src/index.js
+++ src/index.js
@@ -1,46 +1,54 @@
-(function() {
+(function () {
 	var core = require('./asa');
 	var session = require('./session');
-	var partner = require('./partner');	
+	var partner = require('./partner');
 	var autoTrack = require('./auto_track');
 	var debug = require('./debug');
 	var microdata = require('./microdata');
-	
-	var inbox = function inbox(){
 
-		session.extendSession();
+	var inbox = function inbox() {
 
-		if (arguments[0] == 'trackLinks'){
-			autoTrack.links(arguments[1]);
-			return;
+		try {
+			session.extendSession();
+
+			if (arguments[0] == 'trackLinks') {
+				autoTrack.links(arguments[1]);
+				return;
+			}
+			if (arguments[0] == 'debug') {
+				debug.setDebugMode(arguments[1]);
+				return;
+			}
+
+			var event = core.gatherMetaInfo(arguments);
+
+			if (arguments[0] == 'itemview') {
+				event.meta = microdata.extract(arguments[1]);
+			}
+			if (arguments[0] == 'sectionentered') {
+				event.meta = microdata.extract('#' + arguments[1]);
+			}
+
+			core.submitEvent(event);
+		} catch (e) {
+			debug.log('inbox exception:', e);
 		}
-		if (arguments[0] == 'debug'){
-			debug.setDebugMode(arguments[1]);
-			return;
-		}
-
-		var event = core.gatherMetaInfo(arguments);
-
-		if (arguments[0] == 'itemview'){
-			event.meta = microdata.extract(arguments[1]);
-		}
-		if (arguments[0] == 'sectionentered'){
-			event.meta = microdata.extract('#'+arguments[1]);
-		}
-
-		core.submitEvent(event);				
 	};
 
-	var pendingEvents = [];	
-	if (!( typeof window.asa === 'undefined' ) && !( typeof window.asa.q === 'undefined' ) ) {
-		pendingEvents = window.asa.q;
+	try {
+		var pendingEvents = [];
+		if (!(typeof window.asa === 'undefined') && !(typeof window.asa.q === 'undefined')) {
+			pendingEvents = window.asa.q;
+		}
+
+		window.asa = inbox;
+		for (var i = 0; i < pendingEvents.length; i++) {
+			window.asa.apply(null, pendingEvents[i]);
+		}
+
+		partner.setPartnerInfo();
+		autoTrack.sections();
+	} catch (e) {
+		debug.log('exception during init: ', e);
 	}
-	
-	window.asa = inbox;
-	for (var i = 0; i < pendingEvents.length; i++) {
-		window.asa.apply(null, pendingEvents[i]);
-	}
-	
-	partner.setPartnerInfo();	
-	autoTrack.sections();	
 })();
