dee2e1951c0a8d6306cd48b059ad3d4c7162ae0b
Petar Shomov <petar@activitystream.com>
2015-08-18 13:04:32 +0000

    faster source maps for dev mode

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,2 +1,5 @@
 var webpackConf = require('./webpack.config')();
+
+webpackConf.devtool = '#eval-source-map';
+
 module.exports = webpackConf;
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var webpackConf = require('./webpack.config')();
 
+webpackConf.devtool = '#source-map';
 webpackConf.plugins.push(    new webpack.optimize.UglifyJsPlugin({
       mangle: {
         except: ['$super', '$', 'exports', 'require']
