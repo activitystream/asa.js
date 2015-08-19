f45c1752aa5672af2b1aac1779cca68add9b4a82
Petar Shomov <petar@activitystream.com>
2015-08-19 14:23:03 +0000

    avoiding bad user experience on ie8

--- src/debug.js
+++ src/debug.js
@@ -1,3 +1,11 @@
+// old ie
+if (typeof console === 'undefined') {
+	window.console = {};
+}
+if (typeof console.log === 'undefined'){
+	window.console.log = function(){};
+}
+
 var noLog = function noLog() { };
 var doLog = function doLog() {
 	[].unshift.call(arguments, 'asa.js:');
