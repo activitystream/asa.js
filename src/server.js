fe0344c57886e835de0e4c5822bbb84ce49973c9
Petar Shomov <petar@activitystream.com>
2015-08-20 12:03:42 +0000

    ref

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ var batchIntervalHandler;
 
 var postalAddress = '//inbox.activitystream.com/asa';
 
-var post = function(packet, callback){
+var post = function (packet, callback) {
 	var request = new XMLHttpRequest();
 	request.open('POST', postalAddress, true);
 	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
@@ -22,45 +22,45 @@ var post = function(packet, callback){
 	request.onerror = function () {
 		callback(1000, null);
 	};
-	request.send(JSON.stringify(packet));	
-	
+	request.send(JSON.stringify(packet));
+
 }
-var submitNow = function(ev){
+var submitNow = function (ev) {
 	if (!(ev instanceof Array)) ev = [ev];
 	var packet = {
-		ev : ev,
+		ev: ev,
 		t: 1 * new Date()
 	};
-	
+
 	debug.log('submitting event: ', ev);
-	if (features.isExperiment(features.MINI_AJAX)){
-	 post(packet, function(err, _){
-		if (err) {
-			debug.log('error on server', err);
-		} else {
-			debug.log('server got it');
-		}
-	});
-		
-	} else	{
-	  r
-		.post(postalAddress)
-		.set('Content-Type', 'application/json')
-		.send(packet)
-		.end(function (err, res) {
+	if (features.isExperiment(features.MINI_AJAX)) {
+		post(packet, function (err, _) {
 			if (err) {
 				debug.log('error on server', err);
 			} else {
 				debug.log('server got it');
 			}
-		});		
-	}	
+		});
+
+	} else {
+		r
+			.post(postalAddress)
+			.set('Content-Type', 'application/json')
+			.send(packet)
+			.end(function (err, res) {
+				if (err) {
+					debug.log('error on server', err);
+				} else {
+					debug.log('server got it');
+				}
+			});
+	}
 };
-var submitNow2 = function(ev){
+var submitNow2 = function (ev) {
 	done = false;
 	if (!(ev instanceof Array)) ev = [ev];
 	var packet = {
-		ev : ev,
+		ev: ev,
 		t: 1 * new Date()
 	};
 	debug.log('submitting event: ', ev);
@@ -80,26 +80,26 @@ var submitNow2 = function(ev){
 };
 module.exports = {
 	submitEvent: submitNow,
-	batchEvent: function(e){
+	batchEvent: function (e) {
 		pendingSubmission.push(e);
 	},
-	batchOn: function(){
-		batchIntervalHandler = setInterval(function batchProcessor(){
-			try{
+	batchOn: function () {
+		batchIntervalHandler = setInterval(function batchProcessor() {
+			try {
 				if (pendingSubmission.length > 0 && done) {
 					var batchSize = Math.min(pendingSubmission.length, 10);
 					submitNow2(pendingSubmission.slice(0, batchSize));
 				}
-			} catch(e){
-				debug.log('exception submitting', e);				
+			} catch (e) {
+				debug.log('exception submitting', e);
 			}
 		}, 400);
 	},
-	batchOff: function(){
+	batchOff: function () {
 		if (!batchIntervalHandler) {
 			debug.log('cannot batch off, it is not on');
 		} else {
-			clearInterval(batchIntervalHandler);			
-		} 
+			clearInterval(batchIntervalHandler);
+		}
 	}
 };
