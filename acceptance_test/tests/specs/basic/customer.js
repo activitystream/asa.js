dc70bbb94b6298a57af94da92a756621639a8e36
Petar Shomov <petar@activitystream.com>
2015-08-29 22:28:50 +0000

    the e2e tests are starting to shape up

--- acceptance_test/tests/specs/basic/customer.js
+++ acceptance_test/tests/specs/basic/customer.js
@@ -1,6 +1,9 @@
+var expect = require('chai').expect;
 var webdriverio = require('webdriverio');
 var superagent = require('superagent');
 var net = require('net');
+var chai = require('chai');
+var q = require('q');
 
 var host = process.env.HOST || 'localhost';
 var port = process.env.PORT || 4444;
@@ -26,20 +29,32 @@ function retry(done) {
   socket.connect(port, host);
 }
 
-function getLogs(done){
+function getLogs(logs){
   return function(){
-      setTimeout(function(){
+      return q.delay(3000).then(function(){
+        var deferred = q.defer();
         superagent
         .get('http://inbox/log')
         .accept('json')
         .end(function (err, res) {
-          if (err) throw err;
-          console.log('logs:',res.body);
-          done(err); 
+          if (!err) {
+            deferred.resolve(res.body);
+          } else {
+            deferred.reject(err);
+          }
         });
-      }, 1000)
+        return deferred.promise; 
+      });
   }
 }
+
+// before(function() {
+    // var chaiAsPromised = require('chai-as-promised');
+ 
+    // chai.Should();
+    // chai.use(chaiAsPromised);
+    // chaiAsPromised.transferPromiseness = client.transferPromiseness;
+// });
 
 describe('test 1', function () {
   var browser;
@@ -61,5 +76,8 @@ describe('test 1', function () {
     browser
       .url('http://sitea.com')
       .then(getLogs(done))
+      .then(function(eventLogs){
+        expect(eventLogs.length).to.equal(1);
+      }).call(done);
   });
 });
