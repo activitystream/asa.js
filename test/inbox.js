25cb64ed3215fe63d821e177790d87a38407d084
Petar Shomov <petar@activitystream.com>
2015-10-23 13:08:06 +0000

    ref

--- test/inbox.js
+++ test/inbox.js
@@ -8,6 +8,7 @@ var features = require('features');
 describe('inbox', function () {
     var requests = [];
 	var xhr;
+    var asa;
 	var lastRequest = function (keepSession) {
 		var request = JSON.parse(requests[0].requestBody);
 		for (var i = 0; i < request.ev.length; i++) {
@@ -38,6 +39,7 @@ describe('inbox', function () {
         xhr.onCreate = function (xhr) {
             requests.push(xhr);
         };
+        asa = inbox(core.submitEvent);
 	});
 
 	afterEach(function () {
@@ -46,12 +48,12 @@ describe('inbox', function () {
 
 	describe('default pageview', function () {
 		it('should sent only one event', function () {
-			inbox(core.submitEvent)('pageview');
+			asa('pageview');
 
 			expect(requests.length).to.equal(1);
 		})
 		it('should be a POST with data describing the event', function () {
-			inbox(core.submitEvent)('pageview');
+			asa('pageview');
 			var expectation = adjustSystemInfo({ "ev": [{ "type": "pageview", "page": "/test.html", "location": "sadfs", "title": "Opera, Ballett og Konserter | Operaen \\ Den Norske Opera & Ballett", "meta": { "og:description": "Velkommen til Den Norske Opera & Ballett. Her finner du informasjon om våre forestillinger, opera, ballett, konserter og andre kulturtilbud.", "og:url": "http://operaen.no/", "og:title": "Opera, Ballett og Konserter | Operaen  \\ Den Norske Opera & Ballett", "og:site_name": "Operaen.no", "og:type": "website", "keywords": "Den Norske Opera & Ballett, operaen, ballett, nasjonalballetten, nasjonaloperaen, operahuset, konserter, operakoret, operaorkestret, Operaen, forestillinger, operabutikken, opera, Oslo, oslo opera, operaballetten, konserter" } }] });
 
 			expect(lastRequest()).to.eql(expectation);
@@ -61,7 +63,7 @@ describe('inbox', function () {
 	describe('pageview with custom meta', function () {
 		it('should be a POST with data describing the event', function () {
 
-			inbox(core.submitEvent)('pageview', { a: 's' });
+			asa('pageview', { a: 's' });
 
 			var expectation = adjustSystemInfo({ "ev": [{ "type": "pageview", "page": "/test.html", "location": "sadfs", "title": "Opera, Ballett og Konserter | Operaen \\ Den Norske Opera & Ballett", "meta": { "og:description": "Velkommen til Den Norske Opera & Ballett. Her finner du informasjon om våre forestillinger, opera, ballett, konserter og andre kulturtilbud.", "og:url": "http://operaen.no/", "og:title": "Opera, Ballett og Konserter | Operaen  \\ Den Norske Opera & Ballett", "og:site_name": "Operaen.no", "og:type": "website", "keywords": "Den Norske Opera & Ballett, operaen, ballett, nasjonalballetten, nasjonaloperaen, operahuset, konserter, operakoret, operaorkestret, Operaen, forestillinger, operabutikken, opera, Oslo, oslo opera, operaballetten, konserter", "a": "s" } }] });
 
@@ -72,42 +74,42 @@ describe('inbox', function () {
 
 	describe('custom events', function () {
 		it('should send no metadata when none specified', function () {
-			inbox(core.submitEvent)('custom_one');
+			asa('custom_one');
 
 			var expectation = adjustSystemInfo({ "ev": [{ "type": "custom", "event": "custom_one" }] });
 
 			expect(lastRequest()).to.eql(expectation);
 		});
 		it('should send metadata when client id specified as string', function () {
-			inbox(core.submitEvent)('custom_one', 'offer1');
+			asa('custom_one', 'offer1');
 
 			var expectation = adjustSystemInfo({ "ev": [{ "type": "custom", "event": "custom_one", "meta": { "type": "http://schema.org/Offer", "properties": { "name": "Blend-O-Matic", "price": "$19.95", "reviews": { "type": "http://schema.org/AggregateRating", "properties": { "ratingValue": "4", "bestRating": "5", "ratingCount": "25" } } } } }] });
 
 			expect(lastRequest()).to.eql(expectation);
 		});
 		it('should send metadata when specified as DOM element', function () {
-			inbox(core.submitEvent)('custom_one', document.getElementById('offer1'));
+			asa('custom_one', document.getElementById('offer1'));
 
 			var expectation = adjustSystemInfo({ "ev": [{ "type": "custom", "event": "custom_one", "meta": { "type": "http://schema.org/Offer", "properties": { "name": "Blend-O-Matic", "price": "$19.95", "reviews": { "type": "http://schema.org/AggregateRating", "properties": { "ratingValue": "4", "bestRating": "5", "ratingCount": "25" } } } } }] });
 
 			expect(lastRequest()).to.eql(expectation);
 		});
 		it('should send metadata when specified as explicit extra one', function () {
-			inbox(core.submitEvent)('custom_one', { 'a': 's' });
+			asa('custom_one', { 'a': 's' });
 
 			var expectation = adjustSystemInfo({ "ev": [{ "type": "custom", "event": "custom_one", "meta": { 'a': 's' } }] });
 
 			expect(lastRequest()).to.eql(expectation);
 		});
 		it('should send metadata when specified as DOM element and extra metadata', function () {
-			inbox(core.submitEvent)('custom_one', document.getElementById('offer1'), { a: 's' });
+			asa('custom_one', document.getElementById('offer1'), { a: 's' });
 
 			var expectation = adjustSystemInfo({ "ev": [{ "type": "custom", "event": "custom_one", "meta": { "a": "s", "type": "http://schema.org/Offer", "properties": { "name": "Blend-O-Matic", "price": "$19.95", "reviews": { "type": "http://schema.org/AggregateRating", "properties": { "ratingValue": "4", "bestRating": "5", "ratingCount": "25" } } } } }] });
 
 			expect(lastRequest()).to.eql(expectation);
 		});
 		it('should send metadata when specified as DOM element ID and extra metadata', function () {
-			inbox(core.submitEvent)('custom_one', 'offer1', { a: 's' });
+			asa('custom_one', 'offer1', { a: 's' });
 
 			var expectation = adjustSystemInfo({ "ev": [{ "type": "custom", "event": "custom_one", "meta": { "a": "s", "type": "http://schema.org/Offer", "properties": { "name": "Blend-O-Matic", "price": "$19.95", "reviews": { "type": "http://schema.org/AggregateRating", "properties": { "ratingValue": "4", "bestRating": "5", "ratingCount": "25" } } } } }] });
 
@@ -118,7 +120,7 @@ describe('inbox', function () {
 	describe('experiment miniAjax', function () {
 		it('should be a POST with data describing the event', function () {
 			features.defineExperiment(features.MINI_AJAX, 100);
-			inbox(core.submitEvent)('pageview', { a: 's' });
+			asa('pageview', { a: 's' });
 
 			var expectation = adjustSystemInfo({ "ev": [{ "type": "pageview", "page": "/test.html", "location": "sadfs", "title": "Opera, Ballett og Konserter | Operaen \\ Den Norske Opera & Ballett", "meta": { "og:description": "Velkommen til Den Norske Opera & Ballett. Her finner du informasjon om våre forestillinger, opera, ballett, konserter og andre kulturtilbud.", "og:url": "http://operaen.no/", "og:title": "Opera, Ballett og Konserter | Operaen  \\ Den Norske Opera & Ballett", "og:site_name": "Operaen.no", "og:type": "website", "keywords": "Den Norske Opera & Ballett, operaen, ballett, nasjonalballetten, nasjonaloperaen, operahuset, konserter, operakoret, operaorkestret, Operaen, forestillinger, operabutikken, opera, Oslo, oslo opera, operaballetten, konserter", "a": "s" } }] });
 
@@ -146,8 +148,7 @@ describe('inbox', function () {
 	});
     
     describe('custom session management', function(){
-        it('should allow devs to provide their own session', function(){
-            var asa = inbox(core.submitEvent);
+        it('should allow devs to provide their own session id', function(){
             asa('session', function() {return 'my_session';});
             asa('pageview');
             expect(lastRequest(true).ev[0].session).to.equal('my_session');            
