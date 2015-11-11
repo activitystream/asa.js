9492fd191c1cfe6764f7e9e0441fa1721c626760
Petar Shomov <petar@activitystream.com>
2015-11-11 10:22:30 +0000

    extracting the index.js code to boot.js so we can create multiple bundles of the script

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
 (function () {
-    // var DNT = navigator.doNotTrack || navigator.msDoNotTrack || window.doNotTrack;
-    // if (DNT && (DNT === 'yes' || DNT.charAt(0) === '1')) return;
-	var partner = require('./partner');
-	var autoTrack = require('./auto_track');
-	var debug = require('./debug');
-	var inbox = require('./inbox');	
-	var core = require('./server');
-	var features = require('./features');
-
-	try {
-		var pendingEvents = [];
-		if ((typeof window.asa !== 'undefined') && (typeof window.asa.q !== 'undefined')) {
-			pendingEvents = window.asa.q;
-		}
-
-		window.asa = inbox(core.submitEvent);
-
-		// features.defineExperiment(features.MINI_AJAX, 10);
-
-		for (var i = 0; i < pendingEvents.length; i++) {
-			window.asa.apply(null, pendingEvents[i]);
-		}
-
-		partner.setPartnerInfo();
-		autoTrack.sections();
-	} catch (e) {
-		debug.forceLog('exception during init: ', e);
-	}
+    require('./boot')();
 })();
--- src/version.js
+++ src/version.js
@@ -1,7 +1,7 @@
 var features = require('./features');
 var major = 1,
 	minor = 1,
-	build = 8;
+	build = 9;
 module.exports = {
 	major : major,
 	minor: minor,
