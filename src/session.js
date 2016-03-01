48be62a7750991947d7f117958531b343177cf90
Petar Shomov <petar@activitystream.com>
2016-03-01 09:32:50 +0000

    cleaning up a bit

--- src/session.js
+++ src/session.js
@@ -43,16 +43,7 @@ var sessionStore = store;
 var SESSION_EXPIRE_TIMEOUT = 30 * 60;
 // SESSION_EXPIRE_TIMEOUT = 30 * 60;
 var SESSION_COOKIE_NAME = '__asa_session';
-var builtinSessionManager = {
-	extendSession: function () {
-		if (!sessionStore.hasItem(SESSION_COOKIE_NAME)) {
-			debug.log('starting session');
-			sessionStore.setItem(SESSION_COOKIE_NAME, user.getDomainId() + '.' + hash(user.getUserId() + '.' + randomness.getNumber()), SESSION_EXPIRE_TIMEOUT);
-		} else {
-			sessionStore.updateTimeout(SESSION_COOKIE_NAME, SESSION_EXPIRE_TIMEOUT);
-		}
-	},
-    
+var builtinSessionManager = {    
     hasSession: function(){
         return sessionStore.hasItem(SESSION_COOKIE_NAME);
     },
@@ -83,10 +74,6 @@ var providedSessionManager = function(hasSessions, getSession, createSession){
 };
 var sessionManager = builtinSessionManager;
 module.exports = {
-	extendSession: function () {
-        sessionManager.extendSession();
-	},
-
 	getSessionId: function () {
 		return sessionManager.getSessionId();
 	},
@@ -98,5 +85,11 @@ module.exports = {
     },
     customSession : function(hasSessions, getSession, createSession){
         sessionManager = providedSessionManager(hasSessions, getSession, createSession);
+    },
+    saveInSession: function(name, value){
+        store.setItem(name, value);
+    },
+    getFromSession: function(name){
+        return store.getItem(name)
     }
 };
--- src/version.js
+++ src/version.js
@@ -1,7 +1,7 @@
 var features = require('./features');
 var major = 1,
 	minor = 1,
-	build = 42;
+	build = 43;
 module.exports = {
 	major : major,
 	minor: minor,
