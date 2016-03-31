11f43d877d28b2491803230f43373a79d79dd634
Petar Shomov <petar@activitystream.com>
2016-03-31 00:18:22 +0000

    ref

--- src/session.js
+++ src/session.js
@@ -6,81 +6,81 @@ var Cookies = require('./cookies');
 
 
 var persistence = {
-    get : function(id){
+    get: function(id) {
         try {
             return Cookies.getItem(id);
-        } catch(e){
-            throw new Error('Error while trying to get item from session cookie:'+id);
+        } catch (e) {
+            throw new Error('Error while trying to get item from session cookie:' + id);
         }
     },
-    set : function(id, value){
-        try{
+    set: function(id, value) {
+        try {
             return Cookies.setItem(id, value, false, '/');
-        } catch(e){
-            throw new Error('Error while trying to set item to session cookie: "'+id+'" <- '+value);
+        } catch (e) {
+            throw new Error('Error while trying to set item to session cookie: "' + id + '" <- ' + value);
         }
     }
 }
 
 var store = {
-	hasItem: function (name) {
-		var item = persistence.get(name);
-		return item;
-	},
-	getItem: function (name) {
-		return persistence.get(name);
-	},
-	setItem: function (name, value) {
-		persistence.set(name, value);
-	},
+    hasItem: function(name) {
+        var item = persistence.get(name);
+        return item;
+    },
+    getItem: function(name) {
+        return persistence.get(name);
+    },
+    setItem: function(name, value) {
+        persistence.set(name, value);
+    },
 };
 
 var sessionStore = store;
 var SESSION_EXPIRE_TIMEOUT = 30 * 60 * 1000;
 // SESSION_EXPIRE_TIMEOUT = 30 * 60;
 var SESSION_COOKIE_NAME = '__asa_session';
-var builtinSessionManager = {    
-    hasSession: function(){
+var builtinSessionManager = {
+    hasSession: function() {
         var item = sessionStore.hasItem(SESSION_COOKIE_NAME);
         return item && JSON.parse(item).t > (1 * new Date());
     },
-    
-    createSession: function(){
-        sessionStore.setItem(SESSION_COOKIE_NAME, JSON.stringify({ id : user.getDomainId() + '.' + hash(user.getUserId() + '.' + randomness.getNumber()), t : (1*new Date() + SESSION_EXPIRE_TIMEOUT)}));
+
+    createSession: function() {
+        sessionStore.setItem(SESSION_COOKIE_NAME, JSON.stringify({ id: user.getDomainId() + '.' + hash(user.getUserId() + '.' + randomness.getNumber()), t: (1 * new Date() + SESSION_EXPIRE_TIMEOUT) }));
     },
 
-	getSession: function () {
-		return JSON.parse(sessionStore.getItem(SESSION_COOKIE_NAME));
-	}
-    
+    getSession: function() {
+        return JSON.parse(sessionStore.getItem(SESSION_COOKIE_NAME));
+    }
+
 };
-var providedSessionManager = function(hasSessions, getSession, createSession){
+var providedSessionManager = function(hasSessions, getSession, createSession) {
     return {
-        hasSession: function(){
+        hasSession: function() {
             return hasSessions();
         },
-        
-        createSession: function(){
+
+        createSession: function() {
             createSession();
         },
 
-        getSessionId: function () {
+        getSessionId: function() {
             return getSession();
         }
     };
 };
 var sessionManager = builtinSessionManager;
 module.exports = {
-	getSession: function () {
-		return sessionManager.getSession();
-	},
-    hasSession: function(){
+    getSession: function() {
+        return sessionManager.getSession();
+    },
+    hasSession: function() {
         return sessionManager.hasSession();
     },
-    createSession: function(){
+    createSession: function() {
         sessionManager.createSession();
     },
-    customSession : function(hasSessions, getSession, createSession){
+    customSession: function(hasSessions, getSession, createSession) {
         sessionManager = providedSessionManager(hasSessions, getSession, createSession);
     }
 };
