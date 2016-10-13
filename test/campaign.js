1d8db42773e904857272f36be47e88c92c0c8d5a
Petar Shomov <petar@activitystream.com>
2016-10-13 12:44:25 +0000

    adding more tests for campaigns

--- test/campaign.js
+++ test/campaign.js
@@ -6,25 +6,34 @@ var info = require('../src/version');
 var features = require('../src/features');
 var browser = require('../src/browser');
 
-describe('New events', function(){
+describe('Campaigns', function () {
     var originalDocument, originalWindow;
+    var events = [];
 
-    beforeEach(function(){
+    function getNewTab() {
+        return inbox(ev => events.push(ev)); 
+    }
+
+    function emptyEvents() {
+        while(events.length > 0) events.pop();
+    }
+
+    beforeEach(function () {
         originalDocument = browser.document;
         originalWindow = browser.window;
+        events = [];
+        asa = getNewTab();
     })
 
-    afterEach(function(){
+    afterEach(function () {
         browser.document = originalDocument;
-        browser.window = originalWindow;    
+        browser.window = originalWindow;
     })
 
-    it('should have campaign info', function(){
-        var events = []
-        asa = inbox(ev => events.push(ev));
+    it('should have campaign info', function () {
         browser.document = {
-            location : 'http://fle.com/place?utm_campaign=testCampaign&utm_source=testSource',
-            referrer : 'http://smashbangpow.dk',
+            location: 'http://fle.com/place?utm_campaign=testCampaign&utm_source=testSource',
+            referrer: 'http://smashbangpow.dk',
         };
         asa('pageview');
         asa('product.interest', {});
@@ -32,5 +41,31 @@ describe('New events', function(){
         expect(event).to.be.ok;
         expect(event.campaign.campaign).to.equals('testCampaign');
         expect(event.campaign.source).to.equals('testSource');
+        expect(event.page.referrer).to.equals('http://smashbangpow.dk');
+    })
+
+    describe('Resume session', function () {
+        it('opening two different tabs with two different campaigns', function () {
+            browser.document = {
+                location: 'http://fle.com/place?utm_campaign=testCampaign&utm_source=testSource',
+                referrer: 'http://smashbangpow.dk',
+            };
+            asa('pageview');
+            asa('product.interest', {});
+            browser.document = {
+                location: 'http://flo.com/place?utm_campaign=testCampaign1&utm_source=testSource1',
+                referrer: 'http://flipflop.dk',
+            };            
+            emptyEvents();
+            tab2 = getNewTab();
+            tab2('pageview');
+            tab2('product.interest', {});
+            var event = _.find(events, e => e.type === 'product.interest');
+
+            expect(event).to.be.ok;
+            expect(event.campaign.campaign).to.equals('testCampaign1');
+            expect(event.campaign.source).to.equals('testSource1');
+            expect(event.page.referrer).to.equals('http://flipflop.dk');
+        })
     })
 })
