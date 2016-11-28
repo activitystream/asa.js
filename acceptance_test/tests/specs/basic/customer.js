68c95cff1a76dd0b9b496b4c6729f2d23a11cd70
Lozzi <jonatli933@gmail.com>
2016-11-28 17:27:48 +0000

    added tests and logs

--- acceptance_test/tests/specs/basic/customer.js
+++ acceptance_test/tests/specs/basic/customer.js
@@ -19,7 +19,7 @@ var options = {
   port: port,
   user: 'pshomov',
   key: process.env.SAUCE_ACCESS_KEY,
-  
+
   logLevel: 'silent'
 };
 
@@ -50,7 +50,7 @@ function getLogs(){
           }
         });
         return deferred.promise;
-  }
+  };
 }
 
 function wipeLogs(){
@@ -66,6 +66,11 @@ function wipeLogs(){
         });
         return deferred.promise;
 };
+
+var debugLog  = function debugLog(log) {
+    console.log(`Log contains ${JSON.stringify(log, null, 4)}`)
+    return log;
+}
 // before(function() {
     // var chaiAsPromised = require('chai-as-promised');
 
@@ -90,12 +95,133 @@ describe('test 1', function () {
     browser.end();
   });
 
-  it('should send pageview', function (done) {
+  it('should send pageview sitea', function (done) {
     browser
       .url('http://sitea.com')
       .then(getLogs())
+      .then(debugLog, debugLog)
       .then(function(eventLogs){
-        expect(eventLogs.length).to.equal(1);
+        expect(eventLogs.length).to.equal(2);
       }).call(done);
   });
+});
+
+describe('test 2', function () {
+  var browser;
+  beforeEach(function (done) {
+    retry(function () {
+      browser = webdriverio
+        .remote(options)
+        .init().then(wipeLogs);
+      done();
+    });
+
+  });
+
+  afterEach(function () {
+    browser.end();
+  });
+
+  it('should send pageview siteb', function (done) {
+    browser
+      .url('http://siteb.com/buy')
+      .then(getLogs())
+      .then(debugLog, debugLog)
+      .then(function(eventLogs){
+        expect(eventLogs.length).to.equal(2);
+      }).call(done);
+  });
+});
+
+describe('test 3', function () {
+    var browser;
+    beforeEach(function (done) {
+      retry(function () {
+        browser = webdriverio
+          .remote(options)
+          .init().then(wipeLogs);
+        done();
+      });
+
+    });
+
+    afterEach(function () {
+      browser.end();
+    });
+
+    it('sitea submitForm', function (done) {
+      browser
+        .url('http://sitea.com')
+        .submitForm('#_form_')
+        .then(getLogs())
+        .then(debugLog, debugLog)
+        .then(function(eventLogs){
+          expect(eventLogs.length).to.equal(2);
+      }).call(done);
+    });
+
+});
+
+describe('test 4', function () {
+    var browser;
+    beforeEach(function (done) {
+      retry(function () {
+        browser = webdriverio
+          .remote(options)
+          .init().then(wipeLogs);
+        done();
+      });
+
+    });
+
+    afterEach(function () {
+      browser.end();
+    });
+
+    it('sitea click offer1', function (done) {
+      browser
+        .url('http://sitea.com')
+        .click('#_offer1')
+        .then(getLogs())
+        .then(debugLog, debugLog)
+        .then(function(eventLogs){
+          //Expect 4 events, Session Started, Pageview, Session Resumed and itemview
+          expect(eventLogs.length).to.equal(4);
+      }).call(done);
+    });
+
+    it('sitea click twitter', function (done) {
+      browser
+        .url('http://sitea.com')
+        .click('#_twitter')
+        .then(getLogs())
+        .then(debugLog, debugLog)
+        .then(function(eventLogs){
+          expect(eventLogs.length).to.equal(2);
+      }).call(done);
+    });
+
+    it.only('sitea click link with no param', function (done) {
+      browser
+        .url('http://sitea.com')
+        .click('#_link_no_param')
+        .waitForExist('#site_b')
+        .then(getLogs())
+        .then(debugLog, debugLog)
+        .then(function(eventLogs){
+          expect(eventLogs.length).to.equal(4);
+      }).call(done);
+    });
+
+    it('sitea click link with param', function (done) {
+      browser
+        .url('http://sitea.com')
+        .click('#_link_with_param')
+        .waitForExist('#site_b')
+        .then(getLogs())
+        .then(debugLog, debugLog)
+        .then(function(eventLogs){
+          expect(eventLogs.length).to.equal(4);
+      }).call(done);
+    });
 });
