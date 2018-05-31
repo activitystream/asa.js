b1f63d909629eafbc4f8e7da9c28d90169361270
Nikola Pavlovic <nikola.pavlovic@activitystream.com>
2018-05-31 18:28:07 +0200

    add types for product/order data on events, add controlled variant of e2e tests

--- e2e/vnc/index.js
+++ e2e/vnc/index.js
@@ -1,5 +1,8 @@
 import RFB from "@novnc/novnc/core/rfb";
 import socketio from "socket.io-client";
+import COMMAND from "./command";
+
+const CONTROLS = false;
 
 const socket = socketio("http://localhost:8005");
 
@@ -10,8 +13,46 @@ rfb.viewOnly = true;
 
 const panel = document.createElement("div");
 panel.id = "panel";
+const output = document.createElement("div");
+output.id = "output";
 rfb._canvas.parentElement.appendChild(panel);
+if (CONTROLS) {
+  const controls = document.createElement("div");
+  controls.id = "controls";
+  const buttons = [
+    document.createElement("button"),
+    document.createElement("button")
+  ];
+  buttons[0].innerHTML = "<span>Run</span>";
+  buttons[1].innerHTML = "<span>Exit</span>";
+  buttons[0].className = "btn";
+  buttons[0].type = "button";
+  buttons[1].className = "btn red";
+  buttons[1].type = "button";
+
+  controls.appendChild(buttons[0]);
+  controls.appendChild(buttons[1]);
+
+  buttons[0].addEventListener("click", event => {
+    socket.emit("command", COMMAND.RUN);
+  });
+  buttons[1].addEventListener("click", event => {
+    socket.emit("command", COMMAND.EXIT);
+  });
+  panel.appendChild(controls);
+} else {
+  socket.emit("command", COMMAND.RUN);
+}
+panel.appendChild(output);
 
 socket.on("broadcast", data => {
-  panel.innerHTML = panel.innerHTML + data;
+  const item = document.createElement("div");
+  item.innerHTML = data;
+  output.appendChild(item);
+});
+
+socket.on("command", command => {
+  if (command === COMMAND.CLOSE) {
+    window.close();
+  }
 });
